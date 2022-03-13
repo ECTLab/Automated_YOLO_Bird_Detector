bc66fa8d031cbb1fb7ec34fbd3a08f37d582f070
ehsansh75 <101319590+ehsansh75@users.noreply.github.com>
2022-03-13 17:54:03 +0330

    Add files via upload

---
+++ first.py
@@ -0,0 +1,103 @@
+import numpy as np
+import argparse
+import time
+import cv2
+import os
+import keyboard
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-d','--directory',required=True,
+	help="base path directory of images")
+ap.add_argument("-y", "--yolo", required=True,
+	help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applying non-maxima suppression")
+args = vars(ap.parse_args())
+
+labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
+
+weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+directory = f"C:/Users/ehsan/Desktop/project/{args['directory']}"
+run = True
+tmp = 0
+while run:
+	time.sleep(2)
+	count_files = len(os.listdir(directory))
+	if count_files != 0:
+		print(count_files)
+		for img_path in os.listdir(directory):
+			path = os.path.join(f'{directory}/{img_path}')
+			print(path)
+			image = cv2.imread(path)
+			image = cv2.resize(image, (600,400))
+			(H, W) = image.shape[:2]
+			ln = net.getLayerNames()
+			ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+			blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+				swapRB=True, crop=False)
+			net.setInput(blob)
+			start = time.time()
+			layerOutputs = net.forward(ln)
+			end = time.time()
+			print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+
+			boxes = []
+			confidences = []
+			classIDs = []
+
+			for output in layerOutputs:
+				for detection in output:
+					scores = detection[5:]
+					classID = np.argmax(scores)
+					confidence = scores[classID]
+					if confidence > args["confidence"]:
+						box = detection[0:4] * np.array([W, H, W, H])
+						(centerX, centerY, width, height) = box.astype("int")
+						x = int(centerX - (width / 2))
+						y = int(centerY - (height / 2))
+						boxes.append([x, y, int(width), int(height)])
+						confidences.append(float(confidence))
+						classIDs.append(classID)
+			idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+				args["threshold"])
+
+			if len(idxs) > 0:
+				for i in idxs.flatten():
+					(x, y) = (boxes[i][0], boxes[i][1])
+					(w, h) = (boxes[i][2], boxes[i][3])
+					color = [int(c) for c in COLORS[classIDs[i]]]
+					text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+					if LABELS[classIDs[i]] == 'bird':
+						#cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+						#cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+						#	0.5, color, 2)
+						alert = np.zeros((200,700,3)).astype(np.uint8)
+						cv2.putText(alert, 'birds detected turn on the alarms', (0,100), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 1)
+						cv2.imshow('alert',alert)
+
+					else:
+						alert = np.zeros((200,700)).astype(np.uint8)
+						cv2.putText(alert, 'no birds detected', (0,100), cv2.FONT_HERSHEY_COMPLEX, 1, (255,2,2), 1)
+						cv2.imshow('alert',alert)
+			else:
+				alert = np.zeros((200,400,3)).astype(np.uint8)
+				cv2.putText(alert, 'no birds detected', (0,100), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 1)
+				cv2.imshow('alert',alert)
+			cv2.imshow("Image", image)
+			cv2.waitKey(0)
+			cv2.destroyAllWindows()
+			os.remove(path)
+	elif count_files == 0:
+		if keyboard.read_key() == "p":
+			run = False
